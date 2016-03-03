9aa6dffc635229a1256d32126e63a448d9bff379
Jim Soldenwagner <jim.soldenwagner@gmail.com>
2016-03-03 15:40:39 -0800

    initial commit

---
+++ myPoints2.js
@@ -0,0 +1,53 @@
+var myPoints2 = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -119.34637069702148,
+          37.734340111555134
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -119.32027816772461,
+          37.738548538919474
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {},
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -119.32851791381837,
+          37.717096637543165
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "marker-color": "#e8155f",
+        "marker-size": "medium",
+        "marker-symbol": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -119.3305778503418,
+          37.730674512166004
+        ]
+      }
+    }
+  ]
+};
